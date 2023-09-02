864dba7afa1521aa520d8715f283c1069d52f464
muhammadriyansadiq <126410441+muhammadriyansadiq@users.noreply.github.com>
2023-09-02 22:32:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("slides")[0].getElementsByTagName("img");
+
+  if (n >= slides.length) {
+    slideIndex = 0;
+  }
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
+}
